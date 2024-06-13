8f39a7346e6a696271af43fad407a2a46abfe43e
IdenticalSpring <132261017+IdenticalSpring@users.noreply.github.com>
2024-06-14 05:27:36 +0700

    service+model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,18 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderAdminComponent } from '../shared/components/header-admin/header-admin.component';
 import { SidebarAdminComponent } from '../shared/components/sidebar-admin/sidebar-admin.component';
 import { AddNewBlogComponent } from './admin/components/blog/add-new-blog/add-new-blog.component';
+import { DashboardOwnerPageComponent } from './owner/pages/dashboard-owner-page/dashboard-owner-page.component';
+import { CategoryOwnerPageComponent } from './owner/pages/category-owner-page/category-owner-page.component';
+import { ProductOwnerPageComponent } from './owner/pages/product-owner-page/product-owner-page.component';
+import { BrandOwnerPageComponent } from './owner/pages/brand-owner-page/brand-owner-page.component';
+import { BlogOwnerPageComponent } from './owner/pages/blog-owner-page/blog-owner-page.component';
+import { OrderOwnerPageComponent } from './owner/pages/order-owner-page/order-owner-page.component';
+import { DashboardStaffPageComponent } from './staff/pages/dashboard-staff-page/dashboard-staff-page.component';
+import { CategoryStaffPageComponent } from './staff/pages/category-staff-page/category-staff-page.component';
+import { ProductStaffPageComponent } from './staff/pages/product-staff-page/product-staff-page.component';
+import { BrandStaffPageComponent } from './staff/pages/brand-staff-page/brand-staff-page.component';
+import { BlogStaffPageComponent } from './staff/pages/blog-staff-page/blog-staff-page.component';
+import { OrderStaffPageComponent } from './staff/pages/order-staff-page/order-staff-page.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +42,18 @@ import { AddNewBlogComponent } from './admin/components/blog/add-new-blog/add-ne
     FooterComponent,
     HeaderAdminComponent,
     SidebarAdminComponent,
+    DashboardOwnerPageComponent,
+    CategoryOwnerPageComponent,
+    ProductOwnerPageComponent,
+    BrandOwnerPageComponent,
+    BlogOwnerPageComponent,
+    OrderOwnerPageComponent,
+    DashboardStaffPageComponent,
+    CategoryStaffPageComponent,
+    ProductStaffPageComponent,
+    BrandStaffPageComponent,
+    BlogStaffPageComponent,
+    OrderStaffPageComponent,
 
 
   ],
